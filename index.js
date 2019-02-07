7b2d2481c061e01644ded3cbf94b852324ab3c06
Malaj Alexandre <alexandre.malaj@gmail.com>
2019-02-07 10:12:10 +0100

    chore: first code implementation

--- index.js
+++ index.js
@@ -1,11 +1,113 @@
+// Require Node.JS Dependencies
 const {
-    mkdir
-} = require("fs").promises;
+    createWriteStream,
+    promises: {
+        writeFile,
+        mkdir
+    }
+} = require("fs");
+const { isAbsolute, join } = require("path");
+
+// Require Third-Party Dependencies!
+const is = require("@slimio/is");
 const yauzl = require("yauzl");
 
+let UNZIP_DIR = process.cwd();
+let LOG = false;
+let LOG_FILE = false;
 
-function unzip(filePath, options = Object(null)) {
-    return 1;
+function getZipFile(zipFilePath) {
+    return new Promise((resolve, reject) => {
+        yauzl.open(zipFilePath, { lazyEntries: true }, (err, zipfile) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(zipfile);
+        });
+    });
+}
+
+function readStream(zipfile, entry) {
+    zipfile.openReadStream(entry, (err, readStream) => {
+        if (err) {
+            throw err;
+        }
+
+        readStream.on("error", (err) => {
+            throw err;
+        });
+
+        if (LOG) {
+            console.log(`File: ${entry.fileName}`);
+        }
+
+        readStream.on("end", () => {
+            zipfile.readEntry();
+        });
+
+        writeStream(readStream, entry.fileName);
+    });
+}
+
+function writeStream(readStream, fileName) {
+    const write = createWriteStream(join(UNZIP_DIR, fileName));
+    write.on("error", (err) => {
+        throw err;
+    });
+
+    if (LOG_FILE) {
+        readStream.pipe(process.stdout);
+    }
+
+    readStream.pipe(write);
+}
+
+async function createDir(zipfile, entry) {
+    if (LOG) {
+        console.log(`Directory: ${entry.fileName}`);
+    }
+    await mkdir(join(UNZIP_DIR, entry.fileName), { recursive: true });
+    zipfile.readEntry();
+}
+
+function readAllEntries(zipfile) {
+    return new Promise((resolve) => {
+        zipfile.readEntry();
+        zipfile.on("entry", async(entry) => {
+            if (/\/$/.test(entry.fileName)) {
+                await createDir(zipfile, entry);
+            }
+            else {
+                readStream(zipfile, entry);
+            }
+        });
+        zipfile.on("end", () => {
+            resolve();
+        });
+    });
+}
+
+async function unzip(filePath, options = Object.create(null)) {
+    if (!is.nullOrUndefined(options.dir) && !is.string(options.dir)) {
+        throw new TypeError("options.dir param must be a type <string>");
+    }
+    if (!is.nullOrUndefined(options.dir) && !isAbsolute(options.dir)) {
+        throw new TypeError("options.dir param is not an absolute path");
+    }
+
+    if (!is.nullOrUndefined(options.log) && !is.boolean(options.log)) {
+        throw new TypeError("options.log param must be a type <boolean>");
+    }
+    if (!is.nullOrUndefined(options.logFile) && !is.boolean(options.logFile)) {
+        throw new TypeError("options.logFile param must be a type <boolean>");
+    }
+
+    UNZIP_DIR = is.nullOrUndefined(options.dir) ? process.cwd() : options.dir;
+    LOG = is.nullOrUndefined(options.log) ? false : options.log;
+    LOG_FILE = is.nullOrUndefined(options.logFile) ? false : options.logFile;
+
+    const zipFile = await getZipFile(filePath);
+    await readAllEntries(zipFile);
 }
 
 
--- test/test.js
+++ test/test.js
@@ -1,11 +1,14 @@
 // Require Node.JS Dependencies
+const { join } = require("path");
 
 // Require Third-Party Dependencies!
 const avaTest = require("ava");
 const is = require("@slimio/is");
 
 // Require Internal Dependencies
+const unzip = require("../index.js");
 
 avaTest("Bypass", async(assert) => {
+    await unzip(join(__dirname, "/Cache.zip"), { dir: __dirname, log: true });
     assert.pass();
 });
